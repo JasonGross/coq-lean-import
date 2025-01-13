f479bd9bd0bd9f1a36891a0d851d393a418b086d
Cloakless <24829306+Cloakless@users.noreply.github.com>
2025-01-13 23:06:01 +0000

    Update and consolidate tests for Lean 4

--- tests/init.v
+++ tests/init.v
@@ -1,12 +1,3 @@
 From LeanImport Require Import Lean.
 
-Redirect "init1.log" Lean Import "../dumps/init" 1 414836.
-
-Fail Redirect "init2.log" Lean Import "../dumps/init" 414836 414837.
-(* This fails because it expects to reduce a well-founded fixpoint in conversion mode,
-   but the principal argument is Irrelevant so skip_irrelevant_stack does nonsense
-   (mildly not a Coq bug since the fixpoint can only be defined
-   thanks to acc being declared with unchecked univs)
-*)
-
-Redirect "init3.log" Lean Import "../dumps/init" 414837.
+Redirect "init.log" Lean Import "../dumps/init".
--- tests/stdlib.v
+++ tests/stdlib.v
@@ -1,5 +1,3 @@
 From LeanImport Require Import Lean.
 
-Set Lean Error Mode "Skip".
-
 Redirect "stdlib.log" Lean Import "../dumps/stdlib".
